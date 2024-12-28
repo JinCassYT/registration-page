38e7629fefaf4d6d988ddf011bbd9d14a6f65a37
Douglas Aparecido Agostinho <134660696+JinCassYT@users.noreply.github.com>
2024-12-28 16:44:26 -0300

    cadastrar

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form");
+const formName = document.getElementById("formName");
+const formEmail = document.getElementById("formEmail");
+const userName = document.getElementById("userName");
+const formPassword = document.getElementById("formPassword");
+const formPasswordC = document.getElementById("formPasswordC");
+
+confirmbutton.addEventListener("click", (event) => {
+  event.preventDefault();
+  alert(
+    "Parebens,você acaba de se cadastrar,espero que goste e adquira bastante conhecimento. "
+  );
+});
